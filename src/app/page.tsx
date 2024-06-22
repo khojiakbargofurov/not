f70aadc723b3c23aac66257464a8bc647b3098b2
Khojiakbar <harry.gafurov@gmail.com>
2024-06-22 12:40:09 +0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "../../node_modules/next/image";
 import Link from "../../node_modules/next/link";
+interface Product {
+  id: number,
+  title: string,
+  thumbnail: string,
+  images: string[],
+  description: string,
+  price: number,
+  category: string,
 
-const request = async (url : string) => {
+}
+const request = async (url: string) => {
   const req = await fetch(url, {
     next: {
       revalidate: 50,
@@ -15,10 +24,10 @@ const request = async (url : string) => {
 async function Home() {
   const data = await request("https://dummyjson.com/products")
   console.log(data);
-  
+
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 mb-4">
-      {data.products.map((item) => {
+      {data.products.map((item: Product) => {
         return (
           <Link href={`/product/${item.id}`}>
             <div key={item.id} className="card w-64 h-[450px] bg-base-100 shadow-xl mb-4">
@@ -29,9 +38,9 @@ async function Home() {
                 <h2 className="card-title justify-start">
                   {item.title}
                 </h2>
-                <p>{item.description.slice(0,50)} ...</p>
+                <p>{item.description.slice(0, 50)} ...</p>
                 <div className="card-actions justify-end">
-                  <div className="badge badge-outline">{item.category}</div> 
+                  <div className="badge badge-outline">{item.category}</div>
                   <div className="badge badge-outline">{item.price}$</div>
                 </div>
               </div>
